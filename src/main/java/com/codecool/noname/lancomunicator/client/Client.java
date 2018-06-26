420374037ce74ed0ffc117a1b9683ef999e950d3
kamil <siemaeniu500@gmail.com>
2018-06-26 13:17:17 +0200

    Add necessary throws to signature to follow implementations.

--- src/main/java/com/codecool/noname/lancomunicator/client/Client.java
+++ src/main/java/com/codecool/noname/lancomunicator/client/Client.java
@@ -1,7 +1,10 @@
 package com.codecool.noname.lancomunicator.client;
 
+import javax.sound.sampled.LineUnavailableException;
+import java.io.IOException;
+
 public interface Client {
 
-    void startListening();
+    void startListening() throws LineUnavailableException, IOException;
 
 }
