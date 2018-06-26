cf7e64cc533129b83c72c77dd0b7f686f7e537eb
creat73 <nikill1125@gmail.com>
2018-06-26 13:24:08 +0200

    Add audio format creation

--- src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
+++ src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
@@ -1,14 +1,25 @@
 package com.codecool.noname.lancomunicator.server;
 
+import javax.sound.sampled.AudioFormat;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerImpl implements Server {
     private final int port;
-    Socket server;
     private ServerSocket serverSocket;
+    Socket server;
 
     public ServerImpl(int port) {
         this.port = port;
+    }
+
+    private static AudioFormat getAudioFormat() {
+        float sampleRate = 16000.0F;
+        int sampleSizeBits = 16;
+        int channels = 1;
+        boolean signed = true;
+        boolean bigEndian = true;
+
+        return new AudioFormat(sampleRate, sampleSizeBits, channels, signed, bigEndian);
     }
 }
