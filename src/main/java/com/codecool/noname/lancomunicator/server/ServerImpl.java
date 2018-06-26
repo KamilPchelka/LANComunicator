c77fb70b04c9f08bfea6a3eea05a4a41359dfb77
creat73 <nikill1125@gmail.com>
2018-06-26 13:24:43 +0200

    Add broadcast handler

--- src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
+++ src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
@@ -1,6 +1,10 @@
 package com.codecool.noname.lancomunicator.server;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,4 +26,32 @@ public class ServerImpl implements Server {
 
         return new AudioFormat(sampleRate, sampleSizeBits, channels, signed, bigEndian);
     }
+
+    private class broadcastHandler {
+        AudioFormat format = ServerImpl.getAudioFormat();
+        DataLine.Info micInfo = new DataLine.Info(TargetDataLine.class, format);
+        private boolean outVoice = true;
+
+        public void runBroadcast() {
+            try (TargetDataLine mic = (TargetDataLine) AudioSystem.getLine(micInfo)) {
+                server = serverSocket.accept();
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                mic.open(format);
+                System.out.println("Mic open.");
+                byte tmpBuff[] = new byte[mic.getBufferSize() / 5];
+                mic.start();
+                while (outVoice) {
+                    System.out.println("Reading from mic.");
+                    int count = mic.read(tmpBuff, 0, tmpBuff.length);
+                    if (count > 0) {
+                        System.out.println("Writing buffer to server.");
+                        out.write(tmpBuff, 0, count);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
