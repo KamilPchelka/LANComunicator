9e21cf9d704a62cdb63beac4722c602492473aca
kamil <siemaeniu500@gmail.com>
2018-06-27 20:26:55 +0200

    Move playing audio to seperate class.

--- src/main/java/com/codecool/noname/lancomunicator/client/ClientImpl.java
+++ src/main/java/com/codecool/noname/lancomunicator/client/ClientImpl.java
@@ -1,27 +1,23 @@
 package com.codecool.noname.lancomunicator.client;
 
-import com.codecool.noname.lancomunicator.server.ServerImpl;
-
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class ClientImpl implements Client {
 
 
-    private final AudioFormat format;
-    private final InetAddress inetAddress;
+    private final AudioFormat format = AudioPlayerImpl.getAudioFormat();
+    private final AudioPlayer audioPlayer = new AudioPlayerImpl();
     private MulticastSocket socket;
     private boolean listening = true;
 
 
-    public ClientImpl(String hostname, int port) throws IOException {
+    public ClientImpl(int port) throws IOException {
         this.socket = new MulticastSocket(port);
-        this.inetAddress = InetAddress.getByName(hostname);
-        this.format = ServerImpl.getAudioFormat();
 
 
     }
@@ -37,33 +33,13 @@ public class ClientImpl implements Client {
             socket.receive(receivePacket);
             try (ByteArrayInputStream bais = new ByteArrayInputStream(data);
                  AudioInputStream ais = new AudioInputStream(bais, format, data.length)) {
-                int bytesRead = 0;
-                if ((bytesRead = ais.read(data)) != -1) {
-                    new Thread(() -> toSpeaker(data)).start();
+                int bytesRead = ais.read(data);
+                if (bytesRead != -1) {
+                    new Thread(() -> audioPlayer.play(data)).start();
                 }
             }
         }
     }
 
-    public void toSpeaker(byte soundbytes[]) {
-        try {
-
-            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
-            SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-
-            sourceDataLine.open(format);
-
-            sourceDataLine.start();
-
-            System.out.println("format? :" + sourceDataLine.getFormat());
-
-            sourceDataLine.write(soundbytes, 0, soundbytes.length);
-            sourceDataLine.drain();
-            sourceDataLine.close();
-        } catch (Exception e) {
-            System.out.println("Not working in speakers...");
-            e.printStackTrace();
-        }
-    }
 
 }
