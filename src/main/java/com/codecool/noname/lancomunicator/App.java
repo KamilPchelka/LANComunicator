fc5e3eeaf58c87ba888f1c3bff5a43398501a590
kamil <siemaeniu500@gmail.com>
2018-06-27 20:23:09 +0200

    Redesign main class, cause additional arguments are not needed now.

--- src/main/java/com/codecool/noname/lancomunicator/App.java
+++ src/main/java/com/codecool/noname/lancomunicator/App.java
@@ -3,25 +3,22 @@ package com.codecool.noname.lancomunicator;
 import com.codecool.noname.lancomunicator.client.ClientImpl;
 import com.codecool.noname.lancomunicator.server.ServerImpl;
 
-import javax.sound.sampled.LineUnavailableException;
 import java.io.IOException;
 
 public class App {
 
 
-    public static void main(String[] args) throws IOException, LineUnavailableException {
+    public static void main(String[] args) throws IOException {
 
 
         try {
-            String mode = args[1];
+            String mode = args[0];
 
             if (mode.equalsIgnoreCase("server")) {
-                int port = Integer.valueOf(args[2]);
-                new ServerImpl(port).startBroadcasting();
+                new ServerImpl().startBroadcasting();
             } else if (mode.equalsIgnoreCase("client")) {
-                String hostname = args[2];
-                int port = Integer.valueOf(args[3]);
-                new ClientImpl(hostname, port).startListening();
+                int port = Integer.valueOf(args[1]);
+                new ClientImpl(port).startListening();
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("Please provide correct arguments !");
