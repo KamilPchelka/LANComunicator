3e52b3a7926341e1f9356716a850233520df9608
kamil <siemaeniu500@gmail.com>
2018-06-28 11:52:31 +0200

    Implement listening for client connection request.

--- src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
+++ src/main/java/com/codecool/noname/lancomunicator/server/ServerImpl.java
@@ -1,8 +1,10 @@
 package com.codecool.noname.lancomunicator.server;
 
+import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -10,19 +12,42 @@ public class ServerImpl implements Server {
     private final DatagramSocket audioServerSocket;
     private final List<InetAddress> clients = new CopyOnWriteArrayList<>();
 
-    public ServerImpl() throws SocketException {
+    public ServerImpl() throws IOException {
         this.audioServerSocket = new DatagramSocket(0);
+        new ConnectionRequestHanlder().startListening();
+        startBroadcasting();
     }
 
 
-    public void startBroadcasting() throws SocketException {
-        new AudioBroadcastHandler(audioServerSocket, clients).runBroadcast();
-        new VideoBroadcastHandler(clients).runBroadcast();
+    public void startBroadcasting() {
+        /*new AudioBroadcastHandler(audioServerSocket, clients).runBroadcast();
+        new VideoBroadcastHandler(clients).runBroadcast();*/
+
     }
 
     private class ConnectionRequestHanlder {
 
+
+        public void startListening() throws IOException {
+            try (ServerSocket serverSocket = new ServerSocket(9003)) {
+
+                while (true) {
+                    handleRequest(serverSocket.accept());
+                }
+
+            }
+        }
+
+        private void handleRequest(Socket accept) {
+            new Thread(() -> {
+                InetAddress inetAddress = accept.getInetAddress();
+                clients.add(accept.getInetAddress());
+                System.out.printf("Dodano nowego clienta %s:%d \n", inetAddress, accept.getPort());
+            });
+        }
     }
+
+
 
 
 
