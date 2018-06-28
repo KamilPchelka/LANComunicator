56323afd6625e2c612464520b1d67970af0573ab
kamil <siemaeniu500@gmail.com>
2018-06-28 08:01:02 +0200

    Refactor method that updates image view of camera.

--- src/main/java/com/codecool/noname/lancomunicator/gui/ClientGUI.java
+++ src/main/java/com/codecool/noname/lancomunicator/gui/ClientGUI.java
@@ -1,33 +1,29 @@
 package com.codecool.noname.lancomunicator.gui;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.MulticastSocket;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class ClientGUI extends Application {
     ImageView imageView;
     private ObjectProperty<Image> imageProperty = new SimpleObjectProperty<>();
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         Pane pane = new Pane();
         imageView = new ImageView();
         pane.getChildren().add(imageView);
@@ -55,51 +51,24 @@ public class ClientGUI extends Application {
 
     private void startWebCamStream() {
 
-        boolean stopCamera = false;
-        Task<Void> task = new Task<Void>() {
-
-            @Override
-            protected Void call() throws Exception {
-
-                final AtomicReference<WritableImage> ref = new AtomicReference<>();
+        new Thread(() -> {
+            try {
                 byte[] buffer = new byte[50000];
                 MulticastSocket multicastSocket = new MulticastSocket(9002);
-                while (!stopCamera) {
-                    try {
-                        DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+                DatagramPacket dp;
 
-                        multicastSocket.receive(dp);
-                        InputStream in = new ByteArrayInputStream(buffer);
-                        BufferedImage bufferedImage = ImageIO.read(in);
-
-                        DataBuffer dataBuffer = bufferedImage.getData().getDataBuffer();
-
-                        long sizeBytes = ((long) dataBuffer.getSize()) * 4l;
-
-                        ref.set(SwingFXUtils.toFXImage(bufferedImage, ref.get()));
-                        bufferedImage.flush();
-
-
-                        Platform.runLater(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                imageProperty.set(ref.get());
-                            }
-                        });
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                while (true) {
+                    dp = new DatagramPacket(buffer, buffer.length);
+                    multicastSocket.receive(dp);
+                    InputStream in = new ByteArrayInputStream(buffer);
+                    BufferedImage bufferedImage = ImageIO.read(in);
+                    imageProperty.set(SwingFXUtils.toFXImage(bufferedImage, null));
+                    bufferedImage.flush();
                 }
-
-                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        };
-
-        Thread th = new Thread(task);
-        th.start();
-        imageView.imageProperty().bind(imageProperty);
+        }).start();
 
     }
 }
